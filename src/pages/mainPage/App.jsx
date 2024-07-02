b621b373cd4663fa15bb0b66759a5a06ea4aac65
Jean Celin <jean_estudos@hotmail.com>
2024-07-02 09:14:59 -0300

    created API fetch

---
+++ src/pages/mainPage/App.jsx
@@ -0,0 +1,14 @@
+import ApiPrice from "../../api/Api";
+
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <ApiPrice />
+    </div>
+  );
+}
+
+export default App;
