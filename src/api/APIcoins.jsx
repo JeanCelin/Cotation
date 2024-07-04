bdc2357f507a65f78c2bf6fb04c74939f97ace1b
Jean Celin <jean_estudos@hotmail.com>
2024-07-04 12:36:47 -0300

    fixed error with file name

--- src/api/APIcoins.jsx
+++ src/api/APIcoins.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import "./APIcoin.css";
+import "./APIcoins.css";
 import APImath from "./APImath";
 
 export default function ApiCodes() {
